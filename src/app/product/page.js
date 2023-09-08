d68ee7824a1d680d4de9b78a718e69fd82afa555
AbhishekShahi207 <shahiabhi1005@gmail.com>
2023-09-08 23:25:21 +0530

    initial commit

---
+++ src/app/product/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='center'><h1>Studying the File based Routing in Nextjs</h1>
+    <br />
+          <h2>currently you are in Product Page</h2>
+    </div>
+  )
+}
+
+export default page
